051b88239207eaa0423013b5b0cad152a2637b58
alenweeqq <alyona.chuprasova.07@mail.ru>
2026-05-16 19:05:38 +0700

    Update dataset.h

--- dataset.h
+++ dataset.h
@@ -1,25 +1,26 @@
 #ifndef DATASET_H
 #define DATASET_H
 
-#define MAX_LEN_LINE 4096
+#include "tree.h"
+
+#define MAX_LEN_LINE 4096 //макимальная длина строки
 
 typedef struct {                        //структура датасета
     double **X; //матрица признаков
-    int *y; //метки классов
+
+    int *y_class; //массив классов для классификации
+    double *y_reg; //массив значений для регресии
+
     int n_objects; //количество объектов
-    int n_features; //количество признаков 
+    int n_features; //количество признаков
     int num_classes; //количество классов
 } Dataset;
 
-Dataset *load_dataset(const char *filename); //загрузка датасета
+Dataset *load_dataset(const char *filename, Task_Type task_type); //загрузка датасета
 
 void free_dataset(Dataset *ds); //освобождение памяти датасета
 
-void split_dataset(   //разделение датасета на train (обучение) и test (проверка)
-    const Dataset *src,
-    double test_ratio,
-    Dataset **train_out,
-    Dataset **test_out
-);
+void split_dataset(const Dataset *src, double test_ratio,
+                   Dataset **train_out, Dataset **test_out);   //разделение датасета на train (обучение) и test (проверка)
 
 #endif
