4d1b88738794e8c8225a69424cd6865bd1907bb2
alenweeqq <alyona.chuprasova.07@mail.ru>
2026-05-16 19:09:37 +0700

    Update tree.h

--- tree.h
+++ tree.h
@@ -3,11 +3,23 @@
 
 #include <stdio.h>
 
+typedef enum {                          //перечисление критерия разделения
+    CRITERION_GINI, // джини
+    CRITERION_ENTROPY //энтропия
+} Criterion;
+
+typedef enum {                           //перечисление типов задачи
+    TASK_CLASSIFICATION, //классификация
+    TASK_REGRESSION //регрессия
+} Task_Type;
+
+
 typedef struct Node_Tree {                 //структура узла дерева
     int node_leaf; // узел = 0, лист = 1
     int predicted_class; //какой класс предсказывает
+    double predicted_value; //предесказанное значание
     int feature_index; //признак, по которому делим
-    double limit; //предел сравнения 
+    double limit; //предел сравнения
     struct Node_Tree *left; //левый ребенок
     struct Node_Tree *right; //правый ребенок
 } Node_Tree;
@@ -17,17 +29,31 @@ typedef struct {                            //парметры дерева
     int min_objects_split; //минимум объектов для разделения
     int num_features_to_try; //количество случайно выбранных признаков
     int num_classes; // количество классов
+    Criterion criterion; //критерий разделения
+    Task_Type task_type; //тип задачи
 } Params_Tree;
 
 Node_Tree *build_tree(double **X, int *y, int n_objects, int n_features,
-                            int depth, Params_Tree params);                 //рекурсивное построение дерева решений
+                            int depth, Params_Tree params);                    //рекурсивное построение дерева решений (классификация)
 
-int predict_tree(Node_Tree *root, const double *x);  //предсказание дерева
+int predict_tree(Node_Tree *root, const double *x);   //предсказание класса                          //предсказание дерева
+
+Node_Tree *build_regression_tree(double **X, double *y, int n_objects,          //построение дерева регресии
+    int n_features, int depth, Params_Tree params);
+
+double predict_regression_tree(Node_Tree *root, //предсказание значения регрессии
+    const double *x);
 
 void free_tree(Node_Tree *root); //освобождение дерева
 
-void print_tree(Node_Tree *node, int depth); //визуализация дерева
+void print_tree(Node_Tree *node, int depth, Task_Type task_type);       //ASCII вывод дерева
 
-void save_tree(FILE *f, Node_Tree *node);               //сохранение дерева
+void save_tree(FILE *f, Node_Tree *node, Task_Type task_type);      //сохранение дерева
+
+void export_tree_to_dot(Node_Tree *root, const char *filename,
+                        Task_Type task_type);                       //сохранение дерева в .dot
+
+void export_tree_to_png(Node_Tree *root, const char *dot_filename,
+                        const char *png_filename, Task_Type task_type); //сохранение дерева в png
 
 #endif
